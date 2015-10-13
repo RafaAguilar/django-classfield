2f72d021920ba432294acdb1411e38c04b0fbd1e
MikeAmy <cocoade@googlemail.com>
2015-10-13 14:45:32 +0700

    Bump version 1.0.6
    Couldn't seem to overwrite the 1.0.5 release on github

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-classfield',
-    version='1.0.5',
+    version='1.0.6',
     description='Adds a class field to django',
     author='Mike Harris',
     url='https://github.com/mikeharris100/django-classfield',
